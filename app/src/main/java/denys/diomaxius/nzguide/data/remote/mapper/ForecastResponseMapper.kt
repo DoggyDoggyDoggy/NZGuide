b8550fffe95967cf7b90ebbc95d11f9e29e53b45
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-06-14 19:29:02 +1200

    Add mapper for ForecastResponse
    This commit introduces `ForecastResponseMapper.kt` which contains an extension function `toDailyForecastList()`. This function converts a `ForecastResponse` DTO to a list of `DailyForecast` domain models.

---
+++ app/src/main/java/denys/diomaxius/nzguide/data/remote/mapper/ForecastResponseMapper.kt
@@ -0,0 +1,15 @@
+package denys.diomaxius.nzguide.data.remote.mapper
+
+import denys.diomaxius.nzguide.data.remote.dto.ForecastResponse
+import denys.diomaxius.nzguide.domain.model.DailyForecast
+
+fun ForecastResponse.toDailyForecastList(): List<DailyForecast> {
+    return list.map { item ->
+        DailyForecast(
+            date = item.dtTxt,
+            temp = item.main.temp,
+            description = item.weather[0].description,
+            icon = item.weather[0].icon
+        )
+    }
+}
