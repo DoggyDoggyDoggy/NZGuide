b2ced6938969ef2636443a79182c987ca55403ef
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-06-17 21:50:38 +1200

    Implement paging for events
    This commit introduces pagination for fetching events using the Paging 3 library.
    
    Key changes:
    - Created `EventsPagingSource` to handle loading paged data from the `EventsFindApi`. It supports fetching events with or without a `locationId`.
    - Implemented `EventsRepositoryImpl` which uses `EventsPagingSource` to provide `Flow<PagingData<Event>>` for general events and events filtered by location.
    - The `load` function in `EventsPagingSource` fetches data based on the presence of `locationId` and maps the DTOs to domain models.
    - The `getRefreshKey` function in `EventsPagingSource` provides a mechanism for refreshing paged data.

---
+++ app/src/main/java/denys/diomaxius/nzguide/data/remote/paging/EventsPagingSource.kt
@@ -0,0 +1,64 @@
+package denys.diomaxius.nzguide.data.remote.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import denys.diomaxius.nzguide.data.remote.api.EventsFindApi
+import denys.diomaxius.nzguide.data.remote.mapper.toDomain
+import denys.diomaxius.nzguide.domain.model.events.Event
+
+class EventsPagingSource(
+    private val api: EventsFindApi,
+    private val locationId: Int? = null,
+    private val startDate: String?,
+    private val endDate: String?,
+    private val pageSize: Int = 100
+) : PagingSource<Int, Event>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Event> {
+        return try {
+            val offset = params.key ?: 0
+
+            val response = if (locationId == null) {
+                api.getEvents(
+                    rows = pageSize,
+                    offset = offset,
+                    startDate = startDate,
+                    endDate = endDate
+                )
+            } else {
+                api.getEventsByLocation(
+                    location = locationId,
+                    rows = pageSize,
+                    offset = offset,
+                    startDate = startDate,
+                    endDate = endDate
+                    )
+            }
+
+            val items: List<Event> = response.events.map { dto ->
+                dto.toDomain()
+            }
+
+            val nextOffset =
+                if (items.isEmpty() || offset + items.size >= response.attributes.count) {
+                    null
+                } else {
+                    offset + items.size
+                }
+
+            LoadResult.Page(
+                data = items,
+                prevKey = null,
+                nextKey = nextOffset
+            )
+        } catch (ex: Exception) {
+            LoadResult.Error(ex)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Event>): Int? {
+        return state.anchorPosition?.let { anchor ->
+            val page = state.closestPageToPosition(anchor)
+            page?.prevKey ?: page?.nextKey?.minus(pageSize)
+        }
+    }
+}
---
+++ app/src/main/java/denys/diomaxius/nzguide/data/repository/EventsRepositoryImpl.kt
@@ -0,0 +1,68 @@
+package denys.diomaxius.nzguide.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import denys.diomaxius.nzguide.data.remote.mapper.toDomain
+import denys.diomaxius.nzguide.data.remote.api.EventsFindApi
+import denys.diomaxius.nzguide.data.remote.paging.EventsPagingSource
+import denys.diomaxius.nzguide.domain.model.events.Event
+import denys.diomaxius.nzguide.domain.repository.EventsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import kotlin.collections.first
+
+class EventsRepositoryImpl @Inject constructor(
+    private val api: EventsFindApi
+) : EventsRepository {
+
+    override suspend fun getEvent(id: String): Event {
+        val response = api.getEvent(id)
+        return response.events.first().toDomain()
+    }
+
+    override fun getEventsPager(
+        pageSize: Int,
+        startDate: String?,
+        endDate: String?
+    ): Flow<PagingData<Event>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = pageSize,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                EventsPagingSource(
+                    api = api,
+                    locationId = null,
+                    pageSize = pageSize,
+                    startDate = startDate,
+                    endDate = endDate
+                )
+            }
+        ).flow
+    }
+
+    override fun getEventsByLocationPager(
+        location: Int,
+        pageSize: Int,
+        startDate: String?,
+        endDate: String?
+    ): Flow<PagingData<Event>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = pageSize,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                EventsPagingSource(
+                    api = api,
+                    locationId = location,
+                    pageSize = pageSize,
+                    startDate = startDate,
+                    endDate = endDate
+                )
+            }
+        ).flow
+    }
+}
