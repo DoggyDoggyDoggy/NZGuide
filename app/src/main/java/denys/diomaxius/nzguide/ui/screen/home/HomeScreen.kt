c443490a43e3ed4ccd07ae6a315d920a85872757
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-06-19 15:16:49 +1200

    Display city image in CityCard
    This commit updates the `CityCard` composable to display an image of the city.
    
    The image is loaded from the app's assets using `context.assets.open()` and `BitmapFactory.decodeStream()`. The `Image` composable is then used to display the loaded bitmap.

--- app/src/main/java/denys/diomaxius/nzguide/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/denys/diomaxius/nzguide/ui/screen/home/HomeScreen.kt
@@ -1,5 +1,8 @@
 package denys.diomaxius.nzguide.ui.screen.home
 
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -9,6 +12,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import denys.diomaxius.nzguide.domain.model.app.City
 
 @Composable
@@ -20,14 +27,32 @@ fun HomeScreen(
 
     LazyColumn {
         items(cities) {
-            CityCard(city = it)
+            CityCard(
+                city = it
+            )
         }
     }
 }
 
 @Composable
-fun CityCard(city: City) {
+fun CityCard(
+    city: City
+) {
+    val context = LocalContext.current
+    val imageBitmap = remember {
+        context.assets.open(city.photo.first()).use {
+            BitmapFactory.decodeStream(it).asImageBitmap()
+        }
+    }
+
     Card {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            bitmap = imageBitmap,
+            contentDescription = city.cityName,
+            contentScale = ContentScale.FillWidth
+        )
+
         Text(text = city.cityName)
     }
 }
