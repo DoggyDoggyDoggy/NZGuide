371e0ebffbc317bc98dd32d6af935cb4d01da4e6
pedan <pedan.denys@gmail.com>
2025-06-25 00:40:00 +1200

    Refactor: Replace CityPlacesScreen with CardTopThingsToDo in CityScreen
    This commit removes the `CityPlacesScreen` composable and replaces it with two instances of a new `CardTopThingsToDo` composable within a `Row` in the `CityScreen`.
    
    The `CardTopThingsToDo` composable displays a card with the text "Top Things to Do in [cityName] City".

--- app/src/main/java/denys/diomaxius/nzguide/ui/screen/city/CityScreen.kt
+++ app/src/main/java/denys/diomaxius/nzguide/ui/screen/city/CityScreen.kt
@@ -1,11 +1,17 @@
 package denys.diomaxius.nzguide.ui.screen.city
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -16,8 +22,10 @@ import denys.diomaxius.nzguide.ui.components.cityphotoslider.CityPhotoSlider
 import denys.diomaxius.nzguide.ui.components.events.EventsRow
 import denys.diomaxius.nzguide.ui.components.weather.WeatherForecastFiveDays
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import denys.diomaxius.nzguide.navigation.LocalNavController
-import denys.diomaxius.nzguide.ui.components.cityplaces.CityPlacesScreen
 
 @Composable
 fun CityScreen(
@@ -25,7 +33,7 @@ fun CityScreen(
     viewModel: CityScreenViewModel = hiltViewModel()
 ) {
     val city by viewModel.city.collectAsState()
-    val  navHostController = LocalNavController.current
+    val navHostController = LocalNavController.current
 
     Column(
         modifier = modifier
@@ -59,8 +67,55 @@ fun CityScreen(
             modifier = Modifier.height(16.dp)
         )
 
+        /*
         CityPlacesScreen(
             city = city
+        )*/
+
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            CardTopThingsToDo(
+                modifier = Modifier.weight(1f),
+                cityName = city.cityName
+            )
+            CardTopThingsToDo(
+                modifier = Modifier.weight(1f),
+                cityName = city.cityName
+            )
+        }
+    }
+}
+
+@Composable
+fun CardTopThingsToDo(
+    modifier: Modifier = Modifier,
+    cityName: String) {
+    Card(
+        modifier = modifier
+            .padding(12.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            text = "Top Things to Do in $cityName City",
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
