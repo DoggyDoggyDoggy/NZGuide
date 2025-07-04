559ac0081a1281a9e42f2f273e954dc384c21f95
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-04 16:18:36 +1200

    Refactor: Extract CardTopThingsToDo to separate file
    This commit moves the `CardTopThingsToDo` composable function from `CityScreen.kt` to its own file, `CardTopThingsToDo.kt`, within the same package.
    
    Additionally, the `CityScreen` now utilizes a `Scaffold` for its layout, and the main content is extracted into a `Content` composable function.

---
+++ app/src/main/java/denys/diomaxius/nzguide/ui/screen/city/CardTopThingsToDo.kt
@@ -0,0 +1,61 @@
+package denys.diomaxius.nzguide.ui.screen.city
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import denys.diomaxius.nzguide.navigation.NavScreen
+
+@Composable
+fun CardTopThingsToDo(
+    modifier: Modifier = Modifier,
+    cityName: String,
+    navHostController: NavHostController,
+    cityPlacesJsonPath: String,
+    cardText: String
+) {
+    Card(
+        modifier = modifier
+            .heightIn(min = 150.dp)
+            .clickable {
+                navHostController.navigate(
+                    NavScreen.CityPlaces.createRoute(
+                        cityPlacesJsonPath = cityPlacesJsonPath,
+                        cityName = cityName
+                    )
+                ) {
+                    launchSingleTop = true
+                }
+            },
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 150.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = cardText,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
--- app/src/main/java/denys/diomaxius/nzguide/ui/screen/city/CityScreen.kt
+++ app/src/main/java/denys/diomaxius/nzguide/ui/screen/city/CityScreen.kt
@@ -1,21 +1,16 @@
 package denys.diomaxius.nzguide.ui.screen.city
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -26,13 +21,9 @@ import denys.diomaxius.nzguide.ui.components.cityphotoslider.CityPhotoSlider
 import denys.diomaxius.nzguide.ui.components.events.EventsRow
 import denys.diomaxius.nzguide.ui.components.weather.WeatherForecastFiveDays
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import denys.diomaxius.nzguide.domain.model.app.City
 import denys.diomaxius.nzguide.navigation.LocalNavController
-import denys.diomaxius.nzguide.navigation.NavScreen
 
 @Composable
 fun CityScreen(
@@ -42,6 +33,25 @@ fun CityScreen(
     val city by viewModel.city.collectAsState()
     val navHostController = LocalNavController.current
 
+    Scaffold(
+
+    ) { innerPadding ->
+        Content(
+            modifier = modifier.padding(innerPadding),
+            city = city,
+            navHostController = navHostController
+        )
+    }
+
+
+}
+
+@Composable
+fun Content(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController,
+    city: City
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -97,47 +107,6 @@ fun CityScreen(
                 cityPlacesJsonPath = city.cityPlacesTopics,
                 navHostController = navHostController,
                 cardText = "Add something else here"
-            )
-        }
-    }
-}
-
-@Composable
-fun CardTopThingsToDo(
-    modifier: Modifier = Modifier,
-    cityName: String,
-    navHostController: NavHostController,
-    cityPlacesJsonPath: String,
-    cardText: String
-) {
-    Card(
-        modifier = modifier
-            .heightIn(min = 150.dp)
-            .clickable {
-                navHostController.navigate(
-                    NavScreen.CityPlaces.createRoute(
-                        cityPlacesJsonPath = cityPlacesJsonPath,
-                        cityName = cityName
-                    )
-                ) {
-                    launchSingleTop = true
-                }
-            },
-        elevation = CardDefaults.cardElevation(5.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(min = 150.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = cardText,
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(8.dp)
             )
         }
     }
