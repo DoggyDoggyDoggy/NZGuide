e26b9ceb86c75c69578f73b39d7f9ad75baf8c0a
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-08 21:37:52 +1200

    Style CityPlacesScreen with Cards
    This commit enhances the visual presentation of the `CityPlacesScreen` by wrapping each `CityPlaceTopic` in a `Card` composable.
    
    Key changes:
    - Wrapped `CityPlace` content within a `Card` with rounded corners and elevation.
    - Added padding around the content within the `Card`.
    - Centered the `CityPlaceTopic` title.
    - Applied rounded corners to the `AssetImage` using `clip(RoundedCornerShape(8.dp))`.

--- app/src/main/java/denys/diomaxius/nzguide/ui/screen/cityplaces/CityPlacesScreen.kt
+++ app/src/main/java/denys/diomaxius/nzguide/ui/screen/cityplaces/CityPlacesScreen.kt
@@ -10,6 +10,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -19,6 +22,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import denys.diomaxius.nzguide.domain.model.city.CityPlaceTopic
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -68,32 +72,38 @@ fun Content(
 
 @Composable
 fun CityPlace(topic: CityPlaceTopic) {
-    Column {
-        Spacer(
-            modifier = Modifier.height(25.dp)
-        )
+    Card (
+        modifier = Modifier.padding(vertical = 8.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(4.dp)
+    ){
+        Column (
+          modifier = Modifier.padding(8.dp)
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = topic.title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
+            )
 
-        Text(
-            text = topic.title,
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp
-        )
-
-        Spacer(
-            modifier = Modifier.height(5.dp)
-        )
-
-        if (topic.image != "") {
-            AssetImage(topic.image)
             Spacer(
                 modifier = Modifier.height(5.dp)
             )
-        }
 
-        Text(
-            text = topic.paragraph,
-            fontSize = 16.sp
-        )
+            if (topic.image != "") {
+                AssetImage(topic.image)
+                Spacer(
+                    modifier = Modifier.height(5.dp)
+                )
+            }
+
+            Text(
+                text = topic.paragraph,
+                fontSize = 16.sp
+            )
+        }
     }
 }
 
@@ -109,7 +119,7 @@ fun AssetImage(
     }
 
     Image(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)),
         bitmap = imageBitmap.asImageBitmap(),
         contentDescription = null,
         contentScale = ContentScale.FillWidth
