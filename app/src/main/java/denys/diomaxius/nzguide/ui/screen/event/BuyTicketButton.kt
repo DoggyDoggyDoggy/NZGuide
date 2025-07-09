3a2b814682fb254f7dd3c60b7e50f533994994a2
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-07-10 08:23:06 +1200

    Refactor EventDetailsScreen UI
    This commit refactors the UI of the `EventDetailsScreen` by wrapping its content within a `Card` composable.
    
    Key changes:
    - Wrapped the main content of `EventDetailsScreenContent` in a `Card`.
    - Applied padding and corner rounding to the `Card`.
    - Moved modifier properties for the `BuyTicketButton` from the `BuyTicketButton` composable itself to the `EventDetailsScreen` where it's used. This centralizes layout decisions in the screen-level composable.

--- app/src/main/java/denys/diomaxius/nzguide/ui/screen/event/BuyTicketButton.kt
+++ app/src/main/java/denys/diomaxius/nzguide/ui/screen/event/BuyTicketButton.kt
@@ -2,14 +2,10 @@ package denys.diomaxius.nzguide.ui.screen.event
 
 import android.content.Context
 import android.content.Intent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import denys.diomaxius.nzguide.domain.model.events.Event
@@ -21,11 +17,7 @@ fun BuyTicketButton(
     context: Context
 ) {
     Button(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 32.dp)
-            .padding(bottom = 8.dp)
-            .height(50.dp),
+        modifier = modifier,
         onClick = {
             context.startActivity(
                 Intent(Intent.ACTION_VIEW, event.url.toUri())
--- app/src/main/java/denys/diomaxius/nzguide/ui/screen/event/EventDetailsScreen.kt
+++ app/src/main/java/denys/diomaxius/nzguide/ui/screen/event/EventDetailsScreen.kt
@@ -3,18 +3,20 @@ package denys.diomaxius.nzguide.ui.screen.event
 import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -44,6 +46,11 @@ fun EventDetailsScreen(
             },
             bottomBar = {
                 BuyTicketButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp)
+                        .padding(vertical = 8.dp)
+                        .height(50.dp),
                     event = event,
                     context = context
                 )
@@ -62,37 +69,43 @@ fun Content(
     modifier: Modifier = Modifier,
     event: Event
 ) {
-    Column(
+    Card(
         modifier = modifier
-            .fillMaxSize()
             .padding(top = 12.dp)
             .padding(horizontal = 12.dp)
-            .verticalScroll(rememberScrollState())
     ) {
-        Text(
-            text = event.name,
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp
-        )
-
-        Spacer(modifier = Modifier.height(6.dp))
-
-        AsyncImage(
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            model = event.images.images[0].transforms.transforms.last().url,
-            contentScale = ContentScale.FillWidth,
-            contentDescription = "Image"
-        )
+                .padding(vertical = 12.dp)
+                .padding(horizontal = 12.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = event.name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
 
-        Spacer(modifier = Modifier.height(6.dp))
+            Spacer(modifier = Modifier.height(6.dp))
 
-        EventDescription(event)
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp)),
+                model = event.images.images[0].transforms.transforms.last().url,
+                contentScale = ContentScale.FillWidth,
+                contentDescription = "Image"
+            )
 
-        Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(6.dp))
 
-        EventAddress(event)
-        Spacer(modifier = Modifier.height(4.dp))
-        EventDates(event)
+            EventDescription(event)
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            EventAddress(event)
+            Spacer(modifier = Modifier.height(4.dp))
+            EventDates(event)
+        }
     }
 }
